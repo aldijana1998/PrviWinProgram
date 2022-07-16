49384c140ab49a4fbc7abe579231c38050ed55f6
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-15 21:08:32 -0700

    Add project files.

---
+++ PrviWinProgram/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrviWinProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cc = label1.Text;
+            label1.Text = "Super, sistem radi.";
+            MessageBox.Show(this, "Odlično!", "Poruka");
+            label1.Text = cc;
+        }
+    }
+}
